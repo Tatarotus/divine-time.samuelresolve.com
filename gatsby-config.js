a0a18d66ccad2c1a1c9cfb1af390ffe7a0a7687b
Samuel Dantas <sam@samuelresolve.com>
2020-08-31 20:02:25 -0300

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-rest-api",
+      options: {
+        endpoints: ["https://divine-time.vercel.app/time"]
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ]
+}
